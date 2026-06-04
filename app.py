28d95a427eab5f8f934c18297d5a5bd03d193185
AdamJLewis <Adamjlewis1@hotmail.com>
2026-06-04 10:06:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -531,7 +531,7 @@ teams_df = teams_df.reset_index(drop=True)
 
 st.markdown(
     """
-    <div style="height:60px;"></div>
+    <div style="height:25px;"></div>
     """,
     unsafe_allow_html=True
 )
